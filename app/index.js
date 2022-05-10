9dd925ac8624bbe6bb2ea4c6514d116cb38a10f7
shermondevelopment <victorshermon@yahoo.com>
2022-05-10 17:28:28 -0300

    jsonparser in server

--- app/index.js
+++ app/index.js
@@ -2,10 +2,13 @@
 import 'dotenv/config'
 
 /* express */
-import express from 'express'
+import express, { json } from 'express'
 
 /* initialing server */
 const app = express()
+
+/* setting server */
+app.use(json())
 
 
 app.listen(process.env.PORT || 5555, () => console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀🚀`))
