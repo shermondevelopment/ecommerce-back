841c43fdb4043038876bc57ee6c71b4a97cf7676
shermondevelopment <victorshermon@yahoo.com>
2022-05-10 17:19:28 -0300

    running app in port 555

---
+++ app/index.js
@@ -0,0 +1,8 @@
+/* express */
+import express from 'express'
+
+/* initialing server */
+const app = express()
+
+
+app.listen(process.env.PORT || 5555, () => console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀🚀`))
