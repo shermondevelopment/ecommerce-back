f9557fe4c3ea4a9466d7a006379525b315e0fe2b
shermondevelopment <victorshermon@yahoo.com>
2022-05-10 17:30:32 -0300

    adding cors setting

--- app/index.js
+++ app/index.js
@@ -4,11 +4,15 @@ import 'dotenv/config'
 /* express */
 import express, { json } from 'express'
 
+/* cors */
+import cors from 'cors'
+
 /* initialing server */
 const app = express()
 
 /* setting server */
 app.use(json())
+app.unsubscribe(cors())
 
 
 app.listen(process.env.PORT || 5555, () => console.log(`app running in port ${process.env.PORT} 🚀🚀🚀🚀🚀`))
