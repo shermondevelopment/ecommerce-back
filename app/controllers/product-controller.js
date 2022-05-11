4f9b02b3b13df8e1a995309cbe3ace9eaa000b34
shermondevelopment <victorshermon@yahoo.com>
2022-05-11 17:47:35 -0300

    including feat of list products

--- app/controllers/product-controller.js
+++ app/controllers/product-controller.js
@@ -1,3 +1,11 @@
-export const findProducts = (req, res) => {
-  res.send('Ola')
+/* product model */
+import ProductModel from '../schemas/product-schema.js'
+
+export const findProducts = async (req, res) => {
+  try {
+    const findProduct = await ProductModel.find()
+    res.status(200).json(findProduct)
+  } catch (error) {
+    res.status(500).json({ error })
+  }
 }
