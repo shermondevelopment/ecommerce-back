7d6791a41f5bef0b8439b292d1932480ae6dda40
shermondevelopment <victorshermon@yahoo.com>
2022-05-11 17:43:00 -0300

    add column price in schema of product

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,7 @@ const router = Router()
 /* controllers */
 import { findProducts } from '../controllers/product-controller.js'
 
+/* get /products */
 router.get('/products', findProducts)
 
 export default router
--- app/schemas/product-schema.js
+++ app/schemas/product-schema.js
@@ -4,6 +4,7 @@ const ProductSchema = new mongoose.Schema({
   title: { type: String, required: true, min: 10 },
   slug: { type: String, required: true },
   figure: { type: String, required: true },
+  price: { type: Number, required: true },
   should: { type: String, required: true },
   category_id: { type: String },
   size: [String],
