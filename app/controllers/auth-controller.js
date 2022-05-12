dea6c23245a9973286460c52fe201ab686df9034
shermondevelopment <victorshermon@yahoo.com>
2022-05-11 21:48:15 -0300

    merge branch

--- app/controllers/auth-controller.js
+++ app/controllers/auth-controller.js
@@ -1,43 +1,43 @@
 //Dependencies
-import bcrypt from 'bcrypt';
-import { stripHtml } from 'string-strip-html';
-import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'
+import { stripHtml } from 'string-strip-html'
+import jwt from 'jsonwebtoken'
 //Project Modules
-import userModel from '../schemas/user-schema.js';
-import signInValidation from '../validations/signInValidation.js';
+import userModel from '../schemas/user-schema.js'
+import signInValidation from '../validations/signInValidation.js'
 
 export async function signUp(req, res) {
-  const { name, email, password, passwordConfirmation } = req.body;
+  const { name, email, password, passwordConfirmation } = req.body
   try {
     await userModel.create({
       email,
       name: stripHtml(name).result.trim(),
       password: bcrypt.hashSync(password, 10)
-    });
-    res.sendStatus(201);
+    })
+    res.sendStatus(201)
   } catch (error) {
-    console.log(error.message);
-    res.status(500);
+    console.log(error.message)
+    res.status(500)
   }
 }
 
 export async function signIn(req, res) {
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
-  const validation = signInValidation.validate(req.body, { abortEarly: false });
+  const validation = signInValidation.validate(req.body, { abortEarly: false })
   if (validation.error) {
     return res
       .status(422)
-      .send(validation.error.details.map((detail) => detail.message));
+      .send(validation.error.details.map((detail) => detail.message))
   }
   try {
-    const user = await userModel.findOne({ email });
+    const user = await userModel.findOne({ email })
     if (user && bcrypt.compareSync(password, user.password)) {
-      return res.status(200).send('Logado com sucesso');
+      return res.status(200).send('Logado com sucesso')
     }
-    res.status(401).send('Credenciais não foram aceitas.');
+    res.status(401).send('Credenciais não foram aceitas.')
   } catch (error) {
-    console.log(error);
-    res.status(500);
+    console.log(error)
+    res.status(500)
   }
 }
