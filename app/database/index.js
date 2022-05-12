a3545db004774666a6a040456bb393ef8947e9e8
shermondevelopment <victorshermon@yahoo.com>
2022-05-11 22:01:28 -0300

    conenct db in uri

--- app/database/index.js
+++ app/database/index.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
-import 'dotenv/config';
+import mongoose from 'mongoose'
+import 'dotenv/config'
 
-export default mongoose.connect('mongodb://127.0.0.1:27017');
+export default mongoose.connect(process.env.PORT)
