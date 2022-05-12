466f0d05a78344d6448cd561c932c7f1dd3b8feb
shermondevelopment <victorshermon@yahoo.com>
2022-05-11 21:07:28 -0300

    including pagination in /get products

--- app/controllers/product-controller.js
+++ app/controllers/product-controller.js
@@ -12,7 +12,14 @@ import addProductValidation from '../validation/product-validation.js'
 
 export const findProducts = async (req, res) => {
   try {
+    const pageOptions = {
+      page:
+        parseInt(req.query.page, 10) === 1 ? 0 : parseInt(req.query.page, 10),
+      limit: parseInt(req.query.limit, 10) || 10
+    }
     const findProduct = await ProductModel.find()
+      .skip(pageOptions.page * pageOptions.limit)
+      .limit(pageOptions.limit)
     res.status(200).json(findProduct)
   } catch (error) {
     res.status(500).json({ error })
